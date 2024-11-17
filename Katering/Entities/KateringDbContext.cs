d6434c001e7595d6246051d53174dcda1a3f5f61
Bigajka <130694235+Bigajka@users.noreply.github.com>
2024-11-17 17:23:14 +0100

    Projekt startowy + utworzenia tabeli User w bazie danych

---
+++ Katering/Entities/KateringDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Katering.Entities
+{
+    public class KateringDbContext : DbContext
+    {
+        public KateringDbContext(DbContextOptions<KateringDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        // dodawanie kolejnych tablic !!
+    }
+}
