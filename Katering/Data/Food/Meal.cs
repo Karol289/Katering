f1adbf045e1273a97ba1599c11f134c61405a185
Bartosz Faruga <mrruby64@gmail.com>
2025-01-23 13:41:18 +0100

    added admin screens for managing users and meals.

--- Katering/Data/Food/Meal.cs
+++ Katering/Data/Food/Meal.cs
@@ -22,6 +22,8 @@ namespace Katering.Data.Food
         public int? Calories { get; set; }
         public double? Price { get; set; }
         public string? Description { get; set; }
+
+        public Meal() { }   
     }
 
 }
