6c3cfc5ebd70c160ca73786271821de39bfeeaa3
Filip Kudła <taki03@o2.pl>
2025-01-27 19:34:01 +0100

    Update Meal.cs

--- Katering/Data/Food/Meal.cs
+++ Katering/Data/Food/Meal.cs
@@ -12,20 +12,11 @@ namespace Katering.Data.Food
         public int MealId { get; set; }
 
 
-        //[ForeignKey("DietId")]
-        //public Diet? Diet { get; set; }
-
-        //[ForeignKey("MealCategoryId")]
-        //public MealCategory? MealCategory { get; set; }
-
         [ForeignKey("DietId")]
-        public int? DietTypeId { get; set; }
         public Diet? Diet { get; set; }
 
         [ForeignKey("MealCategoryId")]
-        public int? MealCategoryId { get; set; }
         public MealCategory? MealCategory { get; set; }
-
 
         public string? Name { get; set; }
         public int? Calories { get; set; }
