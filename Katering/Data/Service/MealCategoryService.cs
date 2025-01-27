5b4cb72e05a49fcc6d015bc8079e876631d63b11
Filip Kudła <taki03@o2.pl>
2025-01-27 21:09:37 +0100

    mealcategories added

---
+++ Katering/Data/Service/MealCategoryService.cs
@@ -0,0 +1,48 @@
+﻿using Katering.Data.Food;
+using Katering.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Katering.Data.Service
+{
+    public class MealCategoryService : KateringService
+    {
+        public MealCategoryService(IDbContextFactory<KateringDbContext> dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<List<MealCategory>> GetMealCategoriesAsync()
+        {
+            using var context = _dbContext.CreateDbContext();
+            return await context.MealCategories.ToListAsync();
+        }
+
+        public async Task AddMealCategoryAsync(MealCategory category)
+        {
+            using var context = _dbContext.CreateDbContext();
+            context.MealCategories.Add(category);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateMealCategoryAsync(MealCategory category)
+        {
+            using var context = _dbContext.CreateDbContext();
+            var existingCategory = await context.MealCategories.FindAsync(category.MealCategoryId);
+            if (existingCategory != null)
+            {
+                existingCategory.Name = category.Name;
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteMealCategoryAsync(int categoryId)
+        {
+            using var context = _dbContext.CreateDbContext();
+            var category = await context.MealCategories.FindAsync(categoryId);
+            if (category != null)
+            {
+                context.MealCategories.Remove(category);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
